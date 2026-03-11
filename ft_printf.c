29e05e3e94899f42141b0e59bbc96e876fc35a5b
cdric.B <cdric.b@Mac.lan>
2026-03-12 00:15:28 +0100

    ft_printf_v2

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cdric.b <cdric.b@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 23:02:28 by cdric.b           #+#    #+#             */
-/*   Updated: 2026/03/12 00:12:22 by cdric.b          ###   ########.fr       */
+/*   Updated: 2026/03/12 00:14:46 by cdric.b          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,11 @@ static void	case_managment(va_list list, char c, int *r)
 		ft_putnbr_base(va_arg(list, unsigned long), BASE_HEX_UP, 1, r);
 	if (c == 'p')
 	{
-		(*r) += write(1, "0x",2);
+		(*r) += write(1, "0x", 2);
 		ft_putptr_base(va_arg(list, unsigned long), BASE_HEX_LOW, 1, r);
 	}
 	if (c == '%')
-		(*r) += write(1, &c,1);
+		(*r) += write(1, &c, 1);
 }
 
 int	ft_printf(const char *format, ...)
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cdric.b <cdric.b@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 21:51:07 by cdric.b           #+#    #+#             */
-/*   Updated: 2026/03/12 00:13:41 by cdric.b          ###   ########.fr       */
+/*   Updated: 2026/03/12 00:15:23 by cdric.b          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,24 @@
 /*
 
 	format: 
-			•%c Prints a single character.
-			•%s Prints a string (as defined by the common C convention).
-			•%p The void * pointer argument has to be printed in hexadecimal format.
-			•%d Prints a decimal (base 10) number.
-			•%i Prints an integer in base 10.
-			•%u Prints an unsigned decimal (base 10) number.
-			•%x Prints a number in hexadecimal (base 16) lowercase format.
-			•%X Prints a number in hexadecimal (base 16) uppercase format.
-			•%% Prints a percent sign.
+		•%c Prints a single character.
+		•%s Prints a string (as defined by the common C convention).
+		•%p The void * pointer argument has to be printed in hexadecimal format.
+		•%d Prints a decimal (base 10) number.
+		•%i Prints an integer in base 10.
+		•%u Prints an unsigned decimal (base 10) number.
+		•%x Prints a number in hexadecimal (base 16) lowercase format.
+		•%X Prints a number in hexadecimal (base 16) uppercase format.
+		•%% Prints a percent sign.
 
 */
 
 int	main(void)
 {
 	int	r;
+
 	r = 0;
-	assert(printf("hello: %p\n", &r) ==ft_printf("hello: %p\n", &r));
+	assert(printf("hello: %p\n", &r)
+		== ft_printf("hello: %p\n", &r));
 	return (0);
 }
